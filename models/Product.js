7d477b5e94d32c365efae47c32a84ade7db69550
Arjun Singh <arjunkhakh@gmail.com>
2022-04-07 20:12:40 +0100

    creating the product model + login page

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,51 @@
 // Foreign Key For User and Product
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Product extends Model {}
+
+Product.init(
+    {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        description: {
+          type: DataTypes.STRING,
+        },
+        price: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        installationCost: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        paybackAmount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+          },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              isIn: [['solar', 'wind', 'heat']],
+              msg: "Product category must be either 'solar', 'wind' or 'heat'"
+            }
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product',
+      }
+)
