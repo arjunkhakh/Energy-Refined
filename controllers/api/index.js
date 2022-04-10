ce9695afb4d3b9dd7543403c51e934ecb62fe353
Arjun Singh <arjunkhakh@gmail.com>
2022-04-10 22:19:20 +0100

    sorting out the routes folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,3 @@
-const router = require('express').Router();
+const router = require('express').Router()
 
-const heatRoutes = require('./heat');
-const windRoutes = require('./wind');
-const solarRoutes = require('./solar');
-
-router.use('/heat', heatRoutes);
-router.use('/wind', windRoutes);
-router.use('/solar', solarRoutes);
-
-module.exports = router;
+const createUser =  require('./user/creat')
